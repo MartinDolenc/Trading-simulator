13a2bd9b178923a9d981cd32aca3e22d6e99e03a
MartinDolenc <martin.dolenc7@gmail.com>
2017-05-25 23:01:22 +0200

    Add files via upload
    Added stuff

--- GUI.py
+++ GUI.py
@@ -1,14 +1,39 @@
+import Main
 import tkinter as tk
-
 
 okno = tk.Tk()
 
+global denar
 
-gumb = tk.Entry()
-gumb.grid(row=0, column=1)
-oznaka = tk.Label(okno,text='Hello')
+text_input = tk.Entry()
+text_input.grid(row=0, column=1)
+oznaka = tk.Label(okno,text= "" + str(Main.denar) + " $")
 oznaka.grid(row=0, column=0)
+generiraj1 = tk.Button(okno, text="generiraj", command=lambda :generiraj_delnico(text_input.get()))
+generiraj1.grid(row=1, column=0)
+generiraj2 = tk.Button(okno, text="generiraj", command=lambda :generiraj_delnico(text_input.get()))
+generiraj2.grid(row=1, column=1)
+generiraj3 = tk.Button(okno, text="generiraj", command=lambda :generiraj_delnico(text_input.get()))
+generiraj3.grid(row=1, column=2)
 
 
+
+kolicina_nakupa = tk.Entry()
+kolicina_nakupa.grid(row=3, column=3)
+kupi = tk.Button(okno, text="kupi", command=lambda :delnica.nakup(kolicina_nakupa.get()))
+kupi.grid(row=3, column=4)
+
+
+
+
+def generiraj_delnico(oznaka):
+    global delnica
+    delnica = Main.Delnice(1, oznaka, 10, okno)
+
+
+
+
+posodobi = tk.Button(okno, text="posodobi", command=lambda: delnica.posodobi())
+posodobi.grid(row=2, column=0)
 
 okno.mainloop()
--- Main.py
+++ Main.py
@@ -2,54 +2,62 @@ import ystockquote
 import tkinter as tk
 import time
 
-global denar
-denar = 10000
+denar = 10000.0
+canvas_width = 500.0
+canvas_height = 500.0
+canvas_zamik = 10.0
+
 
 class Delnice:
 
-    def __init__(self, lokacija, oznaka, stevilo_delnic):
+    def __init__(self, lokacija, oznaka, stevilo_delnic, okno):
         self.lokacija = lokacija
         self.oznaka = oznaka
         self.stevilo_delnic = stevilo_delnic
         self.trenutna_vrednost = ystockquote.get_last_trade_price(oznaka)
-        self.prejsnje_vrednosti = 5*[0]
+        self.prejsnje_vrednosti = 10*[canvas_zamik/50]
+        self.okno = okno
 
 
-        self.posodobi()
+        #self.posodobi()
 
-    def nakup(self, oznaka, kolicina):
-        denar -= ystockquote.get_last_trade_price(oznaka)*kolicina
+    def prikaz_denarja(self):
+        print(denar)
+
+    def nakup(self, kolicina):
+        global denar
+        denar -= float(ystockquote.get_last_trade_price(self.oznaka))*float(kolicina)
+        print(denar)
 
 
-    def prodaja(self, oznaka, kolicina):
-        denar += ystockquote.get_last_trade_price(oznaka)*kolicina
+    def prodaja(self, kolicina):
+        global denar
+        denar += float(ystockquote.get_last_trade_price(self.oznaka))*float(kolicina)
+        print(denar)
 
-    def graf(self, prejsnje_vrednosti):
-        self.graf = tk.Canvas
+    def Canvas_Graf(self):
+        self.graf = tk.Canvas(self.okno, width=canvas_width, height=canvas_height)
+        self.graf.grid(row=self.lokacija, column=self.lokacija)
+        self.graf.create_line(canvas_zamik, canvas_height-canvas_zamik, canvas_width-canvas_zamik, canvas_height-canvas_zamik, fill="#476042",width=2)
+        self.graf.create_line(canvas_zamik, canvas_height-canvas_zamik, canvas_zamik, canvas_zamik, fill="#476042",width=2)
+        self.graf.create_text(0, 0, text=str((float(self.prejsnje_vrednosti[9])//10)*10 + 10) + " $", anchor="nw")
+        self.graf.create_text(0, canvas_height, text=str((float(self.prejsnje_vrednosti[9])//10)*10) + " $", anchor="sw")
+        self.graf.create_text(canvas_width, 0, text=self.oznaka, anchor="ne")
+
+        # markings on x axis
+        for i in range(11):
+            x = 100 + (i * 30)
+            self.graf.create_text(x, canvas_height, text='%d' % (30 * i), anchor="s")
+
+        for x in range(9):
+            self.graf.create_line(x*50, ((canvas_height - float(self.prejsnje_vrednosti[x]))%10)*50, (x+1)*50, ((canvas_height - float(self.prejsnje_vrednosti[x+1]))%10)*50, fill="#476042", width=2)
 
 
     def posodobi(self):
-        while True:
+        #while True:
             self.trenutna_vrednost = ystockquote.get_last_trade_price(self.oznaka)
-            self.prejsnje_vrednosti = self.prejsnje_vrednosti[1:].append(self.trenutna_vrednost)
-            self.graf(self.prejsnje_vrednosti)
-
-
-            time.sleep(5)
-
-
-
-
-
-
-
-
-def pridobljaj_cene(perioda):
-
-    while True:
-
-        time.sleep(perioda)
-
-
-def pridobi_zadnjo_ceno(oznaka):
-    return ystockquote.get_last_trade_price(oznaka)
+            self.prejsnje_vrednosti = self.prejsnje_vrednosti[1:] + [self.trenutna_vrednost]
+            print(self.prejsnje_vrednosti)
+            self.Canvas_Graf()
+            #time.sleep(10)
+            #self.posodobi()
