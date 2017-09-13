9d798fa9e30467929d14a6c192ff29d16b7fb6d2
MartinDolenc <martin.dolenc7@gmail.com>
2017-09-13 19:46:21 +0200

    Add files via upload

--- Projektna-naloga/GUI.py
+++ Projektna-naloga/GUI.py
@@ -1,5 +1,6 @@
 import Main
 import tkinter as tk
+from tkinter import messagebox
 
 okno = tk.Tk()
 
@@ -15,8 +16,8 @@ oznaka = tk.Label(okno, textvariable = denar_str)
 oznaka.grid(row=0, column=0)
 
 
-generiraj = tk.Button(okno, text="generiraj", command=lambda :generiraj_delnico(text_input.get()))
-generiraj.grid(row=1, column=0)
+generiraj = tk.Button(okno, text="generiraj delnico", command=lambda :generiraj_delnico(text_input.get()))
+generiraj.grid(row=1, column=1)
 
 
 kolicina_nakupa = tk.Entry()
@@ -31,8 +32,24 @@ kupi = tk.Button(okno, text="prodaj", command=lambda :lambda_prodaja())
 kupi.grid(row=4, column=4)
 
 
-posodobi = tk.Button(okno, text="posodobi", command=lambda: delnica.posodobi())
-posodobi.grid(row=2, column=0)
+posodobi = tk.Button(okno, text="posodobi graf", command=lambda: delnica.posodobi())
+posodobi.grid(row=2, column=2)
+
+
+posodobi = tk.Button(okno, text="shrani", command=lambda: shrani())
+posodobi.grid(row=6, column=1)
+
+
+posodobi = tk.Button(okno, text="naloži", command=lambda: nalozi())
+posodobi.grid(row=7, column=1)
+
+
+def shrani():
+    Main.shrani_delnice()
+
+def nalozi():
+    Main.nalozi_delnice()
+    denar_str.set(str(Main.denar) + " $")
 
 
 def lambda_nakup():
@@ -47,7 +64,11 @@ def lambda_prodaja():
 
 def generiraj_delnico(oznaka):
     global delnica
-    delnica = Main.Delnice([1,1], oznaka, okno)
+    global delnice_po_abecedi
+    if oznaka in Main.delnice_po_abecedi:
+        delnica = Main.Delnice([2,1], oznaka, okno)
+    else:
+        messagebox.showinfo("Error", "Neveljavna oznaka")
 
     print(delnica)
 
--- Projektna-naloga/Main.py
+++ Projektna-naloga/Main.py
@@ -6,7 +6,7 @@ from tkinter import messagebox
 denar = 10000.0
 canvas_width = 500.0
 canvas_height = 500.0
-canvas_zamik = 10.0
+canvas_zamik = 50.0
 kolicina_delnic = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 delnice_po_abecedi = ["amd", "aapl", "kof", "fb", "googl", "intc", "logi", "msft", "tsla", "twtr"]
 
@@ -16,14 +16,11 @@ class Delnice:
         self.lokacija = lokacija
         self.oznaka = oznaka
         self.trenutna_vrednost = ystockquote.get_last_trade_price(oznaka)
-        self.prejsnje_vrednosti = 10*[canvas_zamik/50]
+        self.prejsnje_vrednosti = 10*[self.trenutna_vrednost]
         self.okno = okno
-
 
         self.posodobi()
 
-    def prikaz_denarja(self):
-        print(denar)
 
     def nakup(self, kolicina):
         global denar
@@ -43,9 +40,6 @@ class Delnice:
 
         self.posodobi()
 
-        print(denar)
-        print(kolicina_delnic)
-
 
     def prodaja(self, kolicina):
         global denar
@@ -54,46 +48,99 @@ class Delnice:
 
         x = delnice_po_abecedi.index(str(self.oznaka))
 
-        if int(kolicina) < kolicina_delnic[x]:
+        if int(kolicina) <= kolicina_delnic[x]:
 
             denar += cena_prodaje
             denar = round(denar, 2)
 
-            kolicina_delnic[x] -= kolicina
+            kolicina_delnic[x] -= int(kolicina)
         else:
             messagebox.showinfo("Error", "nimate dovolj delnic")
 
         self.posodobi()
 
-        print(denar)
-        print(kolicina_delnic)
 
     def Canvas_Graf(self):
         self.graf = tk.Canvas(self.okno, width=canvas_width, height=canvas_height)
         self.graf.grid(row=self.lokacija[0], column=self.lokacija[1])
         self.graf.create_line(canvas_zamik, canvas_height-canvas_zamik, canvas_width-canvas_zamik, canvas_height-canvas_zamik, fill="#476042",width=2)
         self.graf.create_line(canvas_zamik, canvas_height-canvas_zamik, canvas_zamik, canvas_zamik, fill="#476042",width=2)
-        self.graf.create_text(0, 0, text=str((float(self.prejsnje_vrednosti[9])//10)*10 + 10) + " $", anchor="nw")
-        self.graf.create_text(0, canvas_height, text=str((float(self.prejsnje_vrednosti[9])//10)*10) + " $", anchor="sw")
-        self.graf.create_text(canvas_width, 0, text=self.oznaka, anchor="ne")
+        self.graf.create_text(7, canvas_zamik, text=str((float(self.prejsnje_vrednosti[9])//10)*10 + 10) + " $", anchor="nw")
+        self.graf.create_text(7, canvas_height - canvas_zamik, text=str((float(self.prejsnje_vrednosti[9])//10)*10) + " $", anchor="sw")
+        self.graf.create_text(canvas_width, 0, text=self.oznaka + ": " + str(self.trenutna_vrednost) + "$", anchor="ne")
         self.graf.create_text(canvas_width, 20, text="število delnic: " + str(kolicina_delnic[delnice_po_abecedi.index(str(self.oznaka))]), anchor="ne")
 
+
         # markings on x axis
-        for i in range(11):
-            x = 100 + (i * 30)
-            self.graf.create_text(x, canvas_height, text='%d' % (30 * i), anchor="s")
+        #for i in range(11):
+            #x = 100 + (i * 30)
+            #self.graf.create_text(x, canvas_height, text='%d' % (30 * i), anchor="s")
 
-        for x in range(9):
-            self.graf.create_line(x*50, ((canvas_height - float(self.prejsnje_vrednosti[x]))%10)*50, (x+1)*50, ((canvas_height - float(self.prejsnje_vrednosti[x+1]))%10)*50, fill="#476042", width=2)
+        razlika_na_y_osi = (canvas_height-2*canvas_zamik)/10
 
 
+        for x in range(10,0,-1):
+            self.graf.create_line(45,(canvas_height - canvas_zamik) - razlika_na_y_osi*x, 55, (canvas_height - canvas_zamik) - razlika_na_y_osi*x, fill="#476042", width=2)
+            #self.graf.create_line(50, razlika_na_y_osi * x, canvas_width, razlika_na_y_osi * x, fill="#476042", width=0.5)
+
+
+        for x in range(8):
+            self.graf.create_line(x*50 + canvas_zamik, ((canvas_height - float(self.prejsnje_vrednosti[x]))%10)*50 + canvas_zamik, (x+1)*50 + canvas_zamik, ((canvas_height - float(self.prejsnje_vrednosti[x+1]))%10)*50 + canvas_zamik, fill="#476042", width=2)
 
 
     def posodobi(self):
-        while True:
+        #while True:
             self.trenutna_vrednost = ystockquote.get_last_trade_price(self.oznaka)
             self.prejsnje_vrednosti = self.prejsnje_vrednosti[1:] + [self.trenutna_vrednost]
             self.Canvas_Graf()
             print(self.prejsnje_vrednosti)
-            time.sleep(120)
+            #time.sleep(120)
             #self.posodobi()
+
+
+def shrani_delnice():
+
+    dat = open("shrani.txt", mode="w")
+
+    dat.write(str(kolicina_delnic) + "\n" + str(denar))
+
+    dat.close()
+
+
+def nalozi_delnice():
+
+    global denar
+    global kolicina_delnic
+
+    shranjeno = ""
+
+    shranjeno_samo_števila = []
+
+    dat = open("shrani.txt", mode="r")
+
+    for vrstica in dat:
+
+        shranjeno += vrstica
+
+    a = shranjeno.split("\n")
+
+    b = a[0][1:-1]
+
+    print(b)
+
+    c = b.split(",")
+
+    print(c)
+
+
+    for x in c:
+        shranjeno_samo_števila.append(int(x))
+
+    denar = float(a[1])
+
+    print(shranjeno_samo_števila)
+
+    kolicina_delnic = shranjeno_samo_števila
+
+
+    dat.close()
